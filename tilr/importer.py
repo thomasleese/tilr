137db338af96e96ac093a843719d70c1c5fd6e7c
Thomas Leese <thomas@leese.io>
2017-03-24 10:12:46 +0000

    Add water ignoring

--- tilr/importer.py
+++ tilr/importer.py
@@ -1,4 +1,5 @@
 import io
+import hashlib
 import sqlite3
 
 import boto3
@@ -20,7 +21,15 @@ class Importer:
         'satellite': Jpegoptim(),
     }
 
-    extensions = {'osm': 'png', 'satellite': 'jpg'}
+    extensions = {
+        'osm': 'png',
+        'satellite': 'jpg',
+    }
+
+    water = {
+        'osm': 'c9bc878a43ceba4bb9367aabf87db2f32f1c0789',
+        'satellite': None,
+    }
 
     def __init__(self, db_filename, s3_bucket_name, aws_access_key_id,
                  aws_secret_access_key):
@@ -37,7 +46,8 @@ class Importer:
                 service VARCHAR(15),
                 zoom INTEGER,
                 row INTEGER,
-                column INTEGER
+                column INTEGER,
+                water BOOLEAN
             );
         """)
 
@@ -60,24 +70,33 @@ class Importer:
 
         return list(row[0] for row in self.cursor)
 
-    def set_done_tile(self, service, zoom, row, column):
+    def set_done_tile(self, service, zoom, row, column, water):
         self.cursor.execute("""
-            INSERT INTO tiles VALUES (?, ?, ?, ?)
-        """, [service, zoom, row, column])
+            INSERT INTO tiles VALUES (?, ?, ?, ?, ?)
+        """, [service, zoom, row, column, water])
 
-    def download_tile(self, service, zoom, row, column):
+    def shasum(self, data):
+        m = hashlib.sha1()
+        m.update(data)
+        return m.hexdigest()
+
+    def download_tile(self, service, zoom, row, column, ignore_water=True):
         service_url = self.services[service]
         compressor = self.compressors[service]
         extension = self.extensions[service]
+        water = self.water[service]
 
         url = service_url.format(zoom=zoom, col=column, row=row)
         res = requests.get(url)
 
         if res.status_code == requests.codes.ok:
-            data = compressor.compress(res.content)
-            key = f'tiles/{service}/{zoom}/{row}/{column}.{extension}'
-            self.bucket.upload_fileobj(io.BytesIO(data), key)
-            self.set_done_tile(service, zoom, row, column)
+            is_water = self.shasum(res.content) == water
+            if not is_water or not ignore_water:
+                data = compressor.compress(res.content)
+                key = f'tiles/{service}/{zoom}/{row}/{column}.{extension}'
+                self.bucket.upload_fileobj(io.BytesIO(data), key)
+
+            self.set_done_tile(service, zoom, row, column, is_water)
         else:
             print(f"Cannot download tile: {url}")
 
@@ -92,7 +111,7 @@ class Importer:
             tiles = self.get_done_tiles(service, zoom, row)
             for col in range(0, count):
                 if col not in tiles:
-                    self.download_tile(service, zoom, row, col)
+                    self.download_tile(service, zoom, row, col, ignore_water)
                     uploaded += 1
 
                 bar.numerator += 1
